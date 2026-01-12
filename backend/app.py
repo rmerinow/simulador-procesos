846516f6dfb0d099942206c7d4d2502b05f1745e
rmerinow <rmerinow@gmail.com>
2026-01-12 00:17:38 +0000

    backend flask minimo con app.py funcionando

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,15 @@
-print("Backend Python funcionando correctamente")
-print("Prueba 2 de python")
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "estado": "ok",
+        "mensaje": "Backend Flask funcionando correctamente"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
